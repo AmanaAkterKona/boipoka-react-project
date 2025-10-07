df9407e518d79e1920c821b4d745e85940a8b662
AmanaAkterKona <Proff.kona@gmail.com>
2025-10-07 14:07:19 +0600

    added sweet alert to bookDetails

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,6 +1,14 @@
+
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import { addToStoredDB } from '../../utility/addToDB';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+const MySwal = withReactContent(Swal)
+
+
+
 
 const BookDetails = () => {
     const {id} = useParams();
@@ -18,6 +26,11 @@ const handleMarkAsRead= id =>{
    //array or collection
    //if book already exist the show a alart
    // if book not exist then push in the collection or array
+    MySwal.fire({
+  title: "Good job!",
+  text: "You clicked the button!",
+  icon: "success"
+});
 
    addToStoredDB(id)
 }
